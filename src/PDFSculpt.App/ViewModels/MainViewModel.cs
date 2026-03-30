1eb85c6f1f7a5b322448283e8dbd7886da04a75c
Jacob Bananal <jacob.r.bananal@outlook.com>
2026-03-30 10:54:02 -0700

    Add MainViewModel and wire MVVM binding to MainWindow

---
+++ src/PDFSculpt.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PDFSculpt.App.Commands;
+using System.Windows.Data;
+
+namespace PDFSculpt.App.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _title = "PDFSculpt";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private RelayCommand? _TestCommand;
+        public RelayCommand TestCommand => _TestCommand ??= new RelayCommand(ExecuteTestCommand);
+
+        private void ExecuteTestCommand()
+        {
+            Title = "Button Clicked!";
+        }
+    }
+}

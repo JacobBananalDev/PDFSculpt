c55641264756b406c0e640c6558c833b17f1fa51
Jacob Bananal <jacob.r.bananal@outlook.com>
2026-03-30 11:00:19 -0700

    Add PdfDocument and PdfPage domain models

---
+++ src/PDFSculpt.Core/Models/PdfDocument.cs
@@ -0,0 +1,11 @@
+﻿namespace PDFSculpt.Core.Models
+{
+    public class PdfDocument
+    {
+        public string FilePath { get; set; } = string.Empty;
+
+        public int PageCount { get; set; }
+
+        public List<PdfPage> Pages { get; set; } = new();
+    }
+}
---
+++ src/PDFSculpt.Core/Models/PdfPage.cs
@@ -0,0 +1,11 @@
+﻿namespace PDFSculpt.Core.Models
+{
+    public class PdfPage
+    {
+        public int PageNumber { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+    }
+}
